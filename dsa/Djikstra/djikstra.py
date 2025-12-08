1786151071e16b3821497b8eb14e526849efe2e1
ThevaSanjeiv <7theva@gmail.com>
2025-12-08 11:38:00 +0530

    djikstra

--- dsa/Djikstra/djikstra.py
+++ dsa/Djikstra/djikstra.py
@@ -30,10 +30,12 @@ def dijkstra(V, adj, S):
 # 3 -> []
 V = 4
 adj = [
-    [(1, 4), (2, 7)],
-    [(2, 1), (3, 5)],
-    [(3, 2)],
-    []
+    [(1, 4), (2, 1)],   # edges going out from node 0 → 1, 0 → 2
+    [(3, 1)],           # edge 1 → 3
+    [(1, 2), (3, 5)],   # edges 2 → 1, 2 → 3
+    []                  # node 3 has no outgoing edges
 ]
+
+
 
 print(dijkstra(V, adj, 0)) 
