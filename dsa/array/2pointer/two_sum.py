c92d2fb0453cbcea2cb0fc68543877d42318f6f0
ThevaSanjeiv <7theva@gmail.com>
2025-12-10 15:23:28 +0530

    leetcode

--- dsa/array/2pointer/two_sum.py
+++ dsa/array/2pointer/two_sum.py
@@ -1,4 +1,4 @@
-arr=[1,3,5,7,9]
+arr=[1,3,5,8,9]
 target=10
 left=0
 right=len(arr)-1
@@ -6,10 +6,12 @@ right=len(arr)-1
 while left < right:
     sum1 = arr[left] + arr[right]
     if sum1 == target:
-        print([arr[left], arr[right]],end=" ")
+        print([left+1, right+1],end=" ")
         left+=1
         right-=1
     elif sum1 > target:
         right -= 1 
     else:
-        left += 1     
+        left += 1   
+
+
